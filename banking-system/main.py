c46fc894b3c8834dd211278c0e24050eec1cd990
Valmisson Grizorte <missongrizorte@gmail.com>
2025-02-27 17:07:22 -0300

    feat(banking-system): add transaction limit

--- banking-system/main.py
+++ banking-system/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 RED = '\033[31m'
 GREEN = '\033[32m'
 RESET = '\033[0m'
@@ -5,7 +7,9 @@ RESET = '\033[0m'
 balance = 500
 limit = 500
 withdrawals = 3
+transaction_limit = 10
 extract = []
+date_start = datetime.now()
 
 menu = '''
 ###############################
@@ -17,10 +21,23 @@ menu = '''
 ###############################
 '''
 
+def date_now ():
+    return datetime.now().strftime('%d/%m/%Y %H:%M')
+
 while True:
     print(menu)
     operation = int(input('> '))
     print('')
+
+    if transaction_limit == 0 and operation in (1, 2):
+        now = datetime.now()
+
+        if now - date_start < timedelta(hours=24):
+            print('Você atingiu o limite diário de transações.')
+            continue
+
+        transaction_limit = 10
+        date_start = now
 
     if operation == 1:
         value = int(input('Digite um valor para depósito: '))
@@ -30,7 +47,9 @@ while True:
             continue
         
         balance += value
-        extract.append(f'Depósito: R$ {value:.2f}')
+        transaction_limit -= 1
+
+        extract.append(f'Depósito: R$ {value:.2f}. \nData: {date_now()}')
 
         print(f'{GREEN}\nDepósito realizado com sucesso.{RESET}')
     elif operation == 2:
@@ -52,14 +71,19 @@ while True:
 
         balance -= value
         withdrawals -= 1
-        extract.append(f'Saque: R$ {value:.2f}')
+        transaction_limit -= 1
+
+        extract.append(f'Saque: R$ {value:.2f}. \nData: {date_now()}')
 
         print(f'{GREEN}\nSaque realizado com sucesso.{RESET}')
     elif operation == 3:
         print(f'Saldo: R$ {balance:.2f}')
+
         if len(extract):
-            print('\nExtrato')
-            print('\n'.join(extract))
+            print('\nExtrato:\n')
+            print('\n---\n'.join(extract))
+
+        print(f'\n{date_now()}')
     elif operation == 4:
         break
     else:
